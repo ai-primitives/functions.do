5f24dfc8ecacf5baa6c93d3ebf3c95de9b4d530a
Nathan Clevenger <nateclev@gmail.com>
2025-02-27 03:42:06 -0600

    Refactored root project location

--- app/(payload)/admin/[[...segments]]/page.tsx
+++ app/(payload)/admin/[[...segments]]/page.tsx
@@ -2,7 +2,7 @@
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
 import type { Metadata } from 'next'
 
-import config from '@payload-config'
+import config from 'payload.config'
 import { RootPage, generatePageMetadata } from '@payloadcms/next/views'
 import { importMap } from '../importMap'
 
@@ -15,10 +15,8 @@ type Args = {
   }>
 }
 
-export const generateMetadata = ({ params, searchParams }: Args): Promise<Metadata> =>
-  generatePageMetadata({ config, params, searchParams })
+export const generateMetadata = ({ params, searchParams }: Args): Promise<Metadata> => generatePageMetadata({ config, params, searchParams })
 
-const Page = ({ params, searchParams }: Args) =>
-  RootPage({ config, params, searchParams, importMap })
+const Page = ({ params, searchParams }: Args) => RootPage({ config, params, searchParams, importMap })
 
 export default Page
