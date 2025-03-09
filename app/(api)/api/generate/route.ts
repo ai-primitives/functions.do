d6a262614cf8ae8b24b12d0578ad504ed612cf2e
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-03-09 06:25:34 -0500

    Added auth for api

--- app/(api)/api/generate/route.ts
+++ app/(api)/api/generate/route.ts
@@ -1,7 +1,13 @@
-import generateObject from "@/lib/generateObject"
-
+import config from 'payload.config'
+import { getPayload } from 'payload'
+import generateObject from '@/lib/generateObject'
 
 export async function POST(request: Request) {
+  const payload = await getPayload({ config })
+  const auth = await payload.auth(request)
+  console.log(auth.user)
+  if (!auth.user) return Response.json({ error: 'Unauthorized' }, { status: 401 })
+
   const body = await request.json()
   console.log({ body })
   const object = await generateObject(body).catch(e => ({ error: e.message }))
