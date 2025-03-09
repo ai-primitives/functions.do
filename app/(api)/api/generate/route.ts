9a8db4fba41f30fdd8b9d12bc9422807aa3b32d0
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-03-09 05:22:09 -0500

    Fixed routes

--- app/(api)/api/generate/route.ts
+++ app/(api)/api/generate/route.ts
@@ -3,6 +3,7 @@ import generateObject from "@/lib/generateObject"
 
 export async function POST(request: Request) {
   const body = await request.json()
+  console.log({ body })
   const object = await generateObject(body)
   return Response.json({ object })
 }
