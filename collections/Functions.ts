c9a06d1586af8586d3f54a396debcc896e0486f9
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2025-03-09 07:03:50 -0500

    Updated logic

--- collections/Functions.ts
+++ collections/Functions.ts
@@ -14,7 +14,7 @@ export const Functions: CollectionConfig = {
       { name: 'output', type: 'select', required: true, options: ['Object', 'Text'], defaultValue: 'Object' },
       { name: 'schema', type: 'relationship', relationTo: 'schemas', admin: { condition: (data) => data.output === 'Object' } },
     ]},
-    { name: 'system', type: 'code', admin: { language: 'mdx' } },
+    { name: 'system', type: 'code', admin: { language: 'mdx', editorOptions: { padding: { top: 20, bottom: 20 }} } },
     { name: 'user', type: 'code', admin: { language: 'mdx' } },
   ],
 }
