4d63967e7997f54a5b16956d47e5e8ff88f471f1
Nathan Clevenger <nateclev@gmail.com>
2025-03-16 13:36:33 -0500

    Updated root api

--- app/(api)/route.ts
+++ app/(api)/route.ts
@@ -13,11 +13,23 @@ import yaml from 'yaml'
 
 export const maxDuration = 800
 
+
+
 export const GET = async (request: Request, { params }: { params: Promise<{ slug: string }>; searchParams: Promise<{ [key: string]: string | string[] }> }) => {
   
   const payload = await getPayload({ config })
 
   const { origin, hostname, searchParams } = new URL(request.url)
+
+  const api = {
+    admin: origin + '/admin',
+    api: origin + '/api',
+    functions: origin + '/functions',
+    models: origin + '/models',
+    evaluation: origin + '/evaluation',
+  }
+
+
   const auth = await payload.auth(request)
   console.log(auth.user?.email)
   // const tenant = auth.user?.tenants?.[0]?.id
@@ -25,5 +37,5 @@ export const GET = async (request: Request, { params }: { params: Promise<{ slug
   
   const start = Date.now()
 
-  return Response.json({ message: 'Hello, world!', tenant, user: auth.user?.email })
+  return Response.json({ api, message: 'Hello, world!', tenant, user: auth.user?.email })
 }
